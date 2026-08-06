aaf545b0bfe80f678dd43d74596419c12e7b3732
y4rmer <164941419+y4rmer@users.noreply.github.com>
2026-08-07 02:10:06 +0300

    Update json.js

--- json.js
+++ json.js
@@ -15,9 +15,9 @@ const response = [
         file: "https://ercec.sc.ke/elearning/secondary/f2/Form%202%20geography%20notes.pdf"
     },
     {
-        name: "Meeting Minutes",
-        category: "misc",
-        file: "documents/minutes.pdf"
+        name: "KLB Chem Form 3",
+        category: "books",
+        file: "https://www.dirzon.com/Zon/DownloadStart?target=telegram%3AKLB-Chem-B3%20(3).pdf&token=cfaba6e2e8674527a5caca34ddde36f1&converted=False"
     },
     {
         name: "Education Handbook",
