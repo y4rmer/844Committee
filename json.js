114c7dcce56820700e7ae1c3c3db46c5d2d17beb
y4rmer <164941419+y4rmer@users.noreply.github.com>
2026-08-07 02:00:21 +0300

    Update json.js

--- json.js
+++ json.js
@@ -5,9 +5,9 @@ const response = [
         file: "https://pdfcoffee.com/geog-bk2-mr-ogoti-rm-3-pdf-free.html"
     },
     {
-        name: "844 CBC Comparison",
-        category: "pamphlets",
-        file: "documents/cbc-comparison.pdf"
+        name: "Biology made familiar Q & A",
+        category: "q&a",
+        file: "https://dirzon.com/Zon/DownloadStart?target=telegram%3ABIOLOGY%20MADE%20FAMILIAR%20QNS.pdf&token=6b6b7d3324b34aa8aa3cb7d66960524e&converted=False"
     },
     {
         name: "Frequently Asked Questions",
